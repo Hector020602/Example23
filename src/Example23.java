280d0d454846edbed7b73538674644fcfa5bdcfe
hvm <aluvid073@ieselcaminas.org>
2022-09-23 18:15:40 +0200

    first commit

---
+++ src/Example23.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Example23 {
+    public static void main(String[] argv) {
+        double grade = 0, sum = 0;
+        boolean wasTen = false;
+        int counter = 0;
+
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+
+        while (grade != -1) {
+            grade = inputValue.nextDouble();
+            if (grade != -1) {
+                sum += grade;
+                counter++;
+                if (grade == 10) {
+                    wasTen = true;
+                }
+            }
+        }
+        System.out.println("Average grade: " + sum / counter);
+        if (wasTen) {
+            System.out.println("There was a 10");
+        } else {
+            System.out.println("There was not a 10");
+        }
+    }
+}
